45c6c55d3fab3f48b5e1a4ddde46b7165d3ce05c
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-20 12:40:41 +0900

    feat: google analytics 적용중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,33 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '@/next-seo.config';
+import Script from 'next/script';
+import Head from 'next/head';
+import * as gtag from '../lib/gtag';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
